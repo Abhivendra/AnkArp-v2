7286d5fd0aead3485f5f21495b28a057e7332c47
abhivendra <abhivendrasingh@gmail.com>
2015-05-09 15:34:50 +0530

    first

---
+++ index.php
@@ -0,0 +1,379 @@
+<!DOCTYPE html>
+
+<head>
+		<link rel="shortcut icon" href="images/favicon.ico" >
+    <meta charset="utf-8">
+    <title>AnkArp IT Solutions Pvt Ltd</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width">
+    
+    <!-- Bootstrap Stylesheet -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    
+    <!-- FontAwesome Icons -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
+    
+    <!-- Google Font -->
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700,800' rel='stylesheet' type='text/css'>
+
+    <!-- Normailize Stylesheet -->
+    <link rel="stylesheet" href="css/normalize.min.css">
+
+    <!-- Main Styles -->
+    <link rel="stylesheet" href="css/templatemo_style.css">
+
+    <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+	
+
+</head>
+<body>
+    <!--[if lt IE 7]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+    <![endif]-->
+
+    <header class="site-header" id="top">
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="row">
+                    <div class="navbar-header">
+                        <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#main-menu">
+                            <span class="sr-only">Toggle navigation</span>
+                            <i class="fa fa-bars"></i>
+                        </button>
+                        <div class="logo-wrapper">
+                            <a class="navbar-brand" href="#">
+                                <img src="images/logo.png"/>
+                                <span class="logo-s"></span>
+                            </a>
+                        </div>  
+                    </div>
+                    <div class="collapse navbar-collapse" id="main-menu">
+                        <ul class="nav navbar-nav navbar-right">
+                            <span class="nav-s"></span>
+                            <li class="active"><a href="#top">Home</a></li>
+                            <li><a href="#product-inner">About</a></li>
+                            <li><a href="#portfolio">Services</a></li>
+                            <li><a href="#promotion">Our Team</a></li>
+                            <li><a href="#contact">Contact</a></li>
+                        </ul>
+                    </div>
+                </div> 
+            </div>
+        </nav>
+    </header>
+
+    <div class="top-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="image-holder">
+                        <img src="images/1.png" alt="">
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <h4>We Are</h4>
+                    <h3>AnkArp IT Solutions</h3>
+                    <h5>On Time. On Budget. No Excuse.</h5>
+                    <p>
+We have established ourselves as one of the leading IT Solutions Company in Gwalior, because of our:<br/>
+-High Quality, Unrivalled Customer Care, Our 100% Reliability, Awesome UX/UI.<br/><br/>
+
+Our team of technologists, engineers, and product managers work collaboratively with businesses to develop successful IT products and seamless customer experiences. Whatever it is you need, we can help you build it.</p>
+                    <a href="#contact" class="button">Contact Us</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="products">
+        <div class="container">
+            <div class="row" id="product-inner">
+                <div class="col-md-4 product-item text-center">
+                    
+                    <h4>OUR PROCESS</h4>
+					<p><font color="white">At AnkArp IT solutions Private Limited, 
+					we have a dedicated and experienced team of computer professionals. 
+					Our niche lies in the fact that we thoroughly understand today’s
+					technologies to help you accomplish your business goals. 
+					At the same time, we take into considerations a range of factors
+					such as your business goals, economic budget and time frame.
+					</font><p>
+                </div>
+                <div class="col-md-4 text-center">
+                    <div class="circle">
+                        <h2><br><br><br></br>About Us</h2>
+						<p>WE CREATE AWESOME THINGS FOR GREAT PEOPLE </br></br>
+						Together, we'll do the most astounding </br>work you would ever have imagined</br>
+
+You have dreams. We bring them to life.</p>
+                    </div>
+                </div>
+                <div class="col-md-4 product-item text-center">
+        
+					
+                    <h4>COMPANY</h4>
+					<p><font color="white">AnkArp IT solutions Private Limited is a company that provides professional 
+					customized solutions in web designing , Database and software development. 
+					We deliver premium solutions for a wide range of business throughout the India. 
+					</font></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="portfolio">
+        <div class="container" id="Grid">
+            <div class="row">
+                <div class="col-md-12 text-center title">
+                    <h2>SERVICES</h2>
+                    <span>WE OFFER YOU WHAT WE LOVE TO DO </br> AND WHAT YOU WOULD LOVE TO SEE</span>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <ul class="filters">
+                        <li class="active filter" data-filter="all">All</li>
+                        <li class="filter" data-filter="responsive">Web Development</li>
+                        <li class="filter" data-filter="creative">Application Development</li>
+                        <li class="filter" data-filter="illustration">Software Development</li>
+                        <li class="filter" data-filter="nature">Cloud Services</li>
+                        <li class="filter" data-filter="graphic">UX/UI Design</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-3 col-sm-6 mix responsive">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/1.jpg" alt="">
+                            <!--<div class="overlay">
+                                <a href="images/gallery/1.jpg" data-rel="lightbox">
+                                    <i class="fa fa-search"></i>
+                                </a>
+                            </div>-->
+                        </div>
+                        <h4>Front-End Design</h4>
+                        <span>Web Development</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix creative">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/2.jpg" alt="">
+                            
+                        </div>
+                        <h4>Android Development</h4>
+                        <span>Application Development</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix creative">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/3.jpg" alt="">
+                            
+                        </div>
+                        <h4>iOS Development</h4>
+                        <span>Application Development</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix creative">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/4.jpg" alt="">
+                            
+                        </div>
+                        <h4>Windows Development</h4>
+                        <span>Application Development</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix illustration">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/5.jpg" alt="">
+                           
+                        </div>
+                        <h4>.Net/Java Application</h4>
+                        <span>Software Development</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix nature">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/6.jpg" alt="">
+                          
+                        </div>
+                        <h4>Cloud Based Data Security</h4>
+                        <span>Cloud Services</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix responsive">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/7.jpg" alt="">
+                           
+                        </div>
+                        <h4>Back-End Design</h4>
+                        <span>Web Development</span>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 mix graphic">
+                    <div class="work-item">
+                        <div class="image-holder">
+                            <img src="images/gallery/8.jpg" alt="">
+                           
+                        </div>
+                        <h4>Graphic Design</h4>
+                        <span>UI/UX Design</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div id="bottom-section">
+        <div class="container" id="promotion">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="left-one text-center">
+                        <h1>IDEAS <em>COME</em> FROM THESE <em>MINDS</em></h1>
+                        <div class="image-holder">
+                            <img src="images/4.png" alt="">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="right-one">
+                        <div class="title text-right">
+                            <h3>OUR TEAM</h3>
+                            <span>WE ARE SMALL, BUT A PROFESSIONAL TEAM</span>
+                        </div>
+                        <div class="text-left">
+                            
+                            <ul>
+                                
+								<li>Arpit Dixit  <a href="https://in.linkedin.com/pub/aarpit-dixit/46/b41/250" target=_blank> &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15"> </a></li>
+								<h4>Co-Founder</h4>
+                                <li>Ankita Katare<a href="https://in.linkedin.com/pub/ankita-katare/84/5b8/1b9" target=_blank> &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>Co-Founder</h4>
+                                <li>Keshav Laad<a href="https://in.linkedin.com/in/keshavlaad" target=_blank> &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>Software Developer</h4>
+                                <li>Abhivendra Singh Parihar<a href="https://in.linkedin.com/in/abhivendrasingh" target=_blank> &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>Web Developer</h4>
+                                <li>Anshul Prasad<a href="https://in.linkedin.com/pub/anshul-prasad/99/3a3/1ba" target=_blank > &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>App Developer</h4>
+                                <li>Upendra Sachan<a href="https://in.linkedin.com/in/upendrasinghsachan" target=_blank > &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>Graphic Designer</h4>
+								<li>Ankit Katare<a href="#" target=_blank> &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>Marketing</h4>
+								<li>Anshul Vyas<a href="https://in.linkedin.com/pub/anshul-vyas/68/420/122" target=_blank> &nbsp; &nbsp;<img src="images/linkedin-icon.gif" height="15" width="15">  </a></li>
+								<h4>Java Developer</h4>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-4">
+                  <!--  <div class="video-holder text-center">
+                        <h3>Australia's Gold Coast Video</h3>
+                        <iframe src="http://player.vimeo.com/video/103076330" width="853" height="430" allowfullscreen></iframe>
+                    </div>-->
+                </div>
+            </div>
+        </div>
+        <div class="container" id="contact">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="contact-form">
+                        <h2>CONTACT US</h2>
+                        <span>SEND A MESSAGE</span>
+                        <form action="#" method="post">
+                            <div class="row">
+                                <fieldset class="col-md-6">
+                                    <input type="text" name="name" id="name" placeholder="Name...">
+                                </fieldset>
+                                <fieldset class="col-md-6">
+                                    <input type="email" name="email" id="email" placeholder="Email Address...">
+                                </fieldset>
+                                <fieldset class="col-md-12">
+                                    <textarea name="message" id="message" placeholder="Message..."></textarea>
+                                </fieldset>
+                                <fieldset class="col-md-12">
+                                    <input type="submit" id="button-message" value="Send Message">
+                                </fieldset>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-7">
+                    <div class="right-contact text-center">
+                        <br/><br/><h2><font color="white">WE ARE HIRING</h2>
+						<p><font color="white">Send your resume now at: ankarpitsolutions@gmail.com</p>
+						
+                        <div class="image-holder margin-bottom">
+                            <img src="images/Hiring.png" alt="" height="200" width="400">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-9">
+                	<h3>AnkArp IT Solutions. Rajeev Plaza, Jayendraganj, Gwalior</h3>
+                    <div class="map-holder">
+                        <div id="map_canvas" class="map-canvas"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6">
+                    <p>© 2015  AnkArp IT Solutions Private Limited   &nbsp; &nbsp; &nbsp;    Created By- <a href="http://abhivendra-singh.strikingly.com/" target=_blank>Abhivendra Singh</a></p>
+                </div>
+                <div class="col-md-6 col-sm-6">
+                    <ul class="social">
+                        <li><a href="https://www.facebook.com/Ankarpitsolutions" class="fa fa-facebook"></a></li>
+                        <!--<li><a href="#" class="fa fa-twitter"></a></li>
+                        <li><a href="#" class="fa fa-linkedin"></a></li>-->
+                        <li><a href="https://plus.google.com/115345227360106297814" class="fa fa-google-plus"></a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer>
+    
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+    
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/main.js"></script>    
+    
+
+    <!-- Google Map -->
+    <script src="http://maps.google.com/maps/api/js?sensor=true"></script>
+    <script src="js/jquery.gmap3.min.js"></script>
+    
+    <!-- Google Map Init-->
+    <script type="text/javascript">
+        jQuery(function($){
+            $('#map_canvas').gmap3({
+                marker:{
+                    address: '26.203296, 78.162645' 
+                },
+                    map:{
+                    options:{
+                    zoom: 16,
+                    scrollwheel: false,
+                    streetViewControl : true
+                    }
+                }
+            });
+        });
+    </script>
+
+</body>
+</html>
